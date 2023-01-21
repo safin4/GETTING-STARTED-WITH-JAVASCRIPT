c95a26db28306ec113edc27168d8465e45d9c830
Jubayer Safin <109754142+safin4@users.noreply.github.com>
2023-01-21 20:55:54 +0600

    Add files via upload
    GETTING STARTED WITH JAVASCRIPT

---
+++ GETTING STARTED WITH JAVASCRIPT/conditionals.js
@@ -0,0 +1,24 @@
+var biscuitPrice = 10;
+
+if (biscuitPrice == 8) {
+  console.log("I will eat the biscuits");
+}
+
+else {
+   console.log("Milk tea is better");
+}
+
+var jobPaiso = true;
+var savingAmount = 500000;
+
+if (jobPaiso == true && savingAmount > 200000) {
+  console.log("Aso tomar patri khuji");
+}
+
+else if (jobPaiso == true) {
+    console.log("Accha koi din pore dekhbo ane")
+}
+
+else {
+   console.log("Tor kopale biya nai");
+} 
---
+++ GETTING STARTED WITH JAVASCRIPT/index.js
@@ -0,0 +1,104 @@
+var bananaPrice = 12;
+console.log(typeof bananaPrice);
+
+var seenAftar  = 21;
+console.log(typeof seenAftar);
+
+var myName = "Jubayer Safin";
+console.log(typeof myName);
+
+var isHot = true;
+var isRich = false;
+console.log(typeof isHot);
+
+var sisterName = "Hafsa";
+console.log(typeof sisterName);
+
+var userName = "Jubayer Safin";
+console.log(userName);
+
+var myCountry = "Bangladesh";
+console.log(myCountry);
+
+var promise = "I promise I will work hard to become a programmer";
+console.log(promise.toLowerCase());
+
+var promise = "I promise I will work hard to become a programmer";
+console.log(promise.toUpperCase());
+
+var promise = "I promise I will work hard to become a programmer";
+console.log(promise.indexOf("to"));
+
+var promise = "I promise I will work hard to become a programmer";
+console.log(promise.split(" "));
+
+var promise = "I promise I will work hard to become a programmer";
+console.log(promise.split(""));
+
+var number1 = 0.4;
+var number2  = 0.3;
+var total = number1 + number2;
+total = total.toFixed(1);
+console.log(total);
+
+var price1 = 25;
+var price2 = 35;
+var total = price1 + price2;
+console.log(total);
+
+var price1 = 30;
+var price2 = 25;
+var total = price1 - price2;
+console.log(total);
+
+var price1 = 20;
+var price2 = 25;
+var total = price1 * price2;
+console.log(total);
+
+var price1 = 60;
+var price2 = 15;
+var total = price1 / price2;
+console.log(total);
+
+var price1 = 25;
+var price2 = 35;
+price1++;
+console.log(price1);
+
+var price1 = 25;
+var price2 = 35;
+price2--;
+console.log(price2);
+
+var name = 'Potato';
+var price1 = 30;
+var result = name + ' ' + price1;
+console.log(result);
+
+var number = -40;
+var absoluteNumber = Math.abs(number);
+console.log(absoluteNumber);
+
+var number = 70.3486;
+var result = Math.round(number);
+console.log(result);
+
+var number = 59.5475;
+var result = Math.ceil(number);
+console.log(result);
+
+var number = 90.28337;
+var result = Math.floor(number);
+console.log(result);
+
+var number = 59.5475;
+var result = Math.random(number);
+console.log(result);
+
+var number = Math.random() * 100;
+var result = Math.round(number);
+console.log(result);
+
+var date = new Date("2022-12-24 ");
+console.log(date);
